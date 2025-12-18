cd95fd51198a0937386b589500f207660a4dd7f6
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2025-12-18 12:10:45 +0530

    [Soumya] : updated quotient.java file

--- gcr-Codebase/programming-elements/level-two/quotient.java
+++ gcr-Codebase/programming-elements/level-two/quotient.java
@@ -10,6 +10,6 @@ public class quotient{
     int remainder = number1 % number2 ;
         
     //Display the result
-    System.out.print("The Quotient is " + quotient + "and Reminder is " + remainder + "of two number " + number1 + "and" + number2 );
+    System.out.print("The Quotient is " + quotient + " and Reminder is " + remainder + " of two number " + number1 + " and " + number2 );
     }
 }
