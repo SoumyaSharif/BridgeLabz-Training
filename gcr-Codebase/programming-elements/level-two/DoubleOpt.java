1ae3710491f46ecd689d8f6a5fa6483416074805
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2025-12-18 12:02:06 +0530

    [Soumya] : updated DoubleOpt.java file

--- gcr-Codebase/programming-elements/level-two/DoubleOpt.java
+++ gcr-Codebase/programming-elements/level-two/DoubleOpt.java
@@ -5,15 +5,15 @@ public class DoubleOpt {
     
     //Input using Scanner and assign value in Double variable name
     double a = sc.nextDouble();
-    double b = sc.nextDouble()();
-    double c = sc.nextDouble()();
+    double b = sc.nextDouble();
+    double c = sc.nextDouble();
     
-    double operation1 = a + b *c;
-    double operation2 = a * b + c;
-    double operation3 =  c + a / b;
-    double operation4 =  a % b + c ;
+    double operation1 = (a + b *c );
+    double operation2 = (a * b + c );
+    double operation3 = (c + a / b);
+    double operation4 = (a % b + c );
         
     //Display the result
-    System.out.print("The results of Int Operations are  " + operation1 + " " + operation 2 + " " + operation3 + "and " + operation4  );
+    System.out.print("The results of Int Operations are  " + operation1 + " , " + operation2 + " , " + operation3 + " and " + operation4  );
     }
 }
