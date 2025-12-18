3ffacacda92754f6ce18560e276cbec7ef6373ce
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2025-12-19 00:22:17 +0530

    [Soumya] : added largestOfThree.java file

---
+++ gcr-Codebase/control-flows/level-one/largestOfThree.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class largestOfThree{
+    public static void main(String[] args){
+   
+    //Scanner used for taking input values from user
+    Scanner sc = new Scanner(System.in);
+    int number1 = sc.nextInt();
+    int number2 = sc.nextInt();
+    int number3 = sc.nextInt();
+
+
+    //(+) is evaluated before (&&), so Java gets confused and expects ')' .
+    //Display the results , Since a boolean expression , output is true or false.
+    System.out.print( " is the first number the largest ? " + ((number1 > number2) && (number1 > number3)));
+    }
+}
+
+    
