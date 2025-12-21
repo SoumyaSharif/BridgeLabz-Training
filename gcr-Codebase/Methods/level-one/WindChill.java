05f14ad5b62317165617920d1a35597cebd1fbfc
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2025-12-22 01:06:25 +0530

    [Soumya] : added windchill.java file

---
+++ gcr-Codebase/Methods/level-one/WindChill.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class WindChill{
+
+    //Create a method to calculate temparature
+    public static double calculateWindChill(double temperature, double windSpeed){
+        double windchill = 35.74 + 0.6215 * temperature + (0.4275 * temperature- 35.75) * (Math.pow(windSpeed,0.16));
+        return windchill;
+    }
+
+    public static void main(String[] args){
+ 
+        //Take input from the user
+        Scanner sc = new Scanner(System.in);
+        double temperature= sc.nextDouble();
+        double windSpeed = sc.nextDouble();
+
+        //Display the result
+        System.out.println(" the wind chill temperature given the temperature and wind speed " + calculateWindChill(temperature,windSpeed) );
+       
+    }
+}
+
+    
