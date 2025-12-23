d033e3a8f0cd7ed6b3f3f02f703c478044427e81
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2025-12-23 21:18:43 +0530

    [Soumya] : added MatrixOperations1.java file

---
+++ gcr-Codebase/Methods/level-three/MatrixOperations1.java
@@ -0,0 +1,79 @@
+public class MatrixOperations1 {
+
+    //Create random matrix
+    public static int[][] createRandomMatrix(int rows, int cols) {
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = (int)(Math.random() * 10);
+            }
+        }
+        return matrix;
+    }
+
+    //Add matrices
+    public static int[][] addMatrix(int[][] a, int[][] b) {
+        int[][] result = new int[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                result[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        return result;
+    }
+
+    //Subtract matrices
+    public static int[][] subtractMatrix(int[][] a, int[][] b) {
+        int[][] result = new int[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                result[i][j] = a[i][j] - b[i][j];
+            }
+        }
+        return result;
+    }
+
+    //Multiply matrices
+    public static int[][] multiplyMatrix(int[][] a, int[][] b) {
+        int[][] result = new int[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < a[0].length; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    // Display matrix
+    public static void displayMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] A = createRandomMatrix(2, 2);
+        int[][] B = createRandomMatrix(2, 2);
+
+        System.out.println("Matrix A:");
+        displayMatrix(A);
+
+        System.out.println("Matrix B:");
+        displayMatrix(B);
+
+        System.out.println("Addition:");
+        displayMatrix(addMatrix(A, B));
+
+        System.out.println("Subtraction:");
+        displayMatrix(subtractMatrix(A, B));
+
+        System.out.println("Multiplication:");
+        displayMatrix(multiplyMatrix(A, B));
+    }
+}
