e78ee01dcaa153b387e7dca27dd1df4d7dff5538
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2025-12-24 15:25:42 +0530

    [Soumya] : added SubstringCount.java file

---
+++ gcr-Codebase/Extras/String-Handling/SubstringCount.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SubstringCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the main string: ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter the substring: ");
+        String sub = sc.nextLine();
+
+        int count = 0;
+        int index = 0;
+
+        while ((index = str.indexOf(sub, index)) != -1) {
+            count++;
+            index = index + 1;
+        }
+
+        System.out.println("Substring occurs " + count + " times");
+    }
+}
