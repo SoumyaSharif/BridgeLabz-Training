fbf58e52b77a4e3523535295f28993f640895ede
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2025-12-27 11:52:02 +0530

    [Soumya]: added Employee.java file

---
+++ java-oops-practice/gcr-code-base/javaOopsConcepts/src/Com/classandobject/level-one/Employee.java
@@ -0,0 +1,28 @@
+package com.classandobject.levelone;
+
+public class Employee {
+
+    // non-static variables
+    String name;
+    int id;
+    double salary;
+
+    // Constructor
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // method to display employee details
+    public void displayEmployee() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("Rohan", 1, 500000);
+        emp.displayEmployee();
+    }
+}
