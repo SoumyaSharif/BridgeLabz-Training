f83822b2f6a65044b3ae0e1c5ba288e52fd7ce41
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2026-01-10 01:15:44 +0530

    [Soumya] : added sortingalgorithms folder

---
+++ dsa-practice/gcr-code-base/coredsapractice/src/coredsapractice/com/sortingalgorithms/InsertionSortEmployeeIDs.java
@@ -0,0 +1,17 @@
+package coredsapractice.com.sortingalgorithms;
+
+class InsertionSortEmployeeIDs {
+
+    public static void insertionSort(int[] empIds) {
+        for (int i = 1; i < empIds.length; i++) {
+            int key = empIds[i];
+            int j = i - 1;
+
+            while (j >= 0 && empIds[j] > key) {
+                empIds[j + 1] = empIds[j];
+                j--;
+            }
+            empIds[j + 1] = key;
+        }
+    }
+}
---
+++ dsa-practice/gcr-code-base/coredsapractice/src/coredsapractice/com/sortingalgorithms/SelectionSortExamScores.java
@@ -0,0 +1,22 @@
+package coredsapractice.com.sortingalgorithms;
+
+class SelectionSortExamScores {
+
+    public static void selectionSort(int[] scores) {
+        int n = scores.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (scores[j] < scores[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = scores[minIndex];
+            scores[minIndex] = scores[i];
+            scores[i] = temp;
+        }
+    }
+}
