47d372cbc014bdc81b4bb8111acc67973abe1a6d
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2026-02-16 12:55:15 +0530

    [Soumya] : added csv to java objects folder

---
+++ gcr-code-base-practice/javaiostreamconcepts/src/com/csv/csvtojavaobjects/ConvertCSVToObjects.java
@@ -0,0 +1,5 @@
+package com.csv.csvtojavaobjects;
+
+public class ConvertCSVToObjects {
+
+}
---
+++ gcr-code-base-practice/javaiostreamconcepts/src/com/csv/csvtojavaobjects/Student.java
@@ -0,0 +1,37 @@
+package com.csv.csvtojavaobjects;
+
+// Represents student data from CSV file
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private double marks;
+    
+    public Student(int id, String name, int age, double marks) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public double getMarks() {
+        return marks;
+    }
+    
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "', age=" + age + ", marks=" + marks + "}";
+    }
+}
